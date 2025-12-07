572882f3f29029eaa85db27122dffedd269b72fc
Gabriel20064 <gabrielhernandez20064@gmail.com>
2025-12-07 18:23:35 -0400

    071225-0623: Validacion Part1

--- src/Cl_vTransaccion.ts
+++ src/Cl_vTransaccion.ts
@@ -31,8 +31,10 @@ export default class vTransaccion extends Cl_vGeneral {
         if (!this.inFecha.value || !this.inDescripcion.value || !this.inMonto.value || !this.inReferencia.value || !this.inTipoTransaccion.value || !this.inCategoria.value){ 
             alert("Debes llenar todos los campos.");
             return;}
-        if (+this.inMonto.value <= 0){ alert("El monto debe ser mayor a 0.");
-        return;}
+        if (+this.inMonto.value <= 0){ 
+            alert("El monto debe ser mayor a 0.");
+            return;
+        }
     const data = {
         fecha: this.inFecha.value,
         descripcion: this.inDescripcion.value.toLowerCase(),
