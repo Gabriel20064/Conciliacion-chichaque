ce3f29d5a66362a717ad8508062f36656bb2f7f0
AbrahamV <1001.31467069.ucla@gmail.com>
2025-12-07 20:16:57 -0400

    071225-816-data

--- src/Cl_index.ts
+++ src/Cl_index.ts
@@ -3,7 +3,7 @@ import Cl_mBanco from "./Cl_mBanco.js";
 import Cl_vBanco from "./Cl_vBanco.js";
 import Cl_vTransaccion from "./Cl_vTransaccion.js";
 import Cl_vEditTransaccion from "./Cl_vEditTransaccion.js";
-
+import {dtTransacciones} from "./Cl_controlador.js";
 
 export default class Cl_index {
   constructor() {
@@ -17,6 +17,9 @@ export default class Cl_index {
     let controlador = new Cl_controlador(
         modelo, vista, vTransaccion, vEditTransaccion
     );
+  dtTransacciones.forEach((transaccion) => {
+    
+  })
     
     vista.controlador = controlador;
     vTransaccion.controlador = controlador;
